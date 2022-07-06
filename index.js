21055b08cd8276ab26476e156cde5218cd6b4d16
Zion Miller <djzionmiller@gmail.com>
2022-07-05 21:06:06 -0700

    commit tree e25d8c325d850e7eda89b7083b2a58984c89518a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+// ✓ returns an employee with the original key value pairs and the new key value pair
+// ✓ it does not modify the original employee, but rather returns a clone with the new data
+const employee = {
+    name: "Bob",
+    streetAddress: "123 A Road, LA, CA, 12345 USA",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return{
+    ...employee,
+    [key]: value,
+    };
+}
+
+// ✓ updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+// ✓ deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+// ✓ does not modify the original employee (it is non-destructive)
+
+function deleteFromEmployeeByKey(employee, key){
+    var otherEmployee = Object.assign({}, employee);
+    delete otherEmployee[key];
+    return otherEmployee;
+};
+
+// returns employee without the deleted key/value pair
+// modifies the original employee
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+};
